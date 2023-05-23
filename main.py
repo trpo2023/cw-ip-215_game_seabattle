8a1bf37280077800f9824c7d8d97b03df35f38a0
Kirill Davydov <kirill_d1581@mail.ru@users.noreply.github.com>
2023-05-24 01:16:35 +0700

    Add exit function and app icon

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import tkinter
 from tkinter import *
 from tkinter import messagebox
 
 win = Tk()
 win.title("Морской бой")
 
+app_running = True
 size_m = 500
 size_n = 500
 
@@ -12,5 +14,20 @@ canvas.create_rectangle(0, 0, size_m, size_n, fill="white")
 canvas.pack()
 win.update()
 
-win.mainloop()
+# функция нового окна закрытия окна
+def on_closing():
+    global app_running
+    if messagebox.askokcancel("Выход из игры", "Хотите выйти из игры?"):
+        app_running = False
+        win.destroy()
 
+
+# функция для иконы
+def icons():
+    icons = tkinter.PhotoImage(file='battleship.png')
+    win.iconphoto(False, icons)
+
+
+icons()
+win.protocol("WM_DELETE_WINDOW", on_closing)
+win.mainloop()
