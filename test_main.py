5799c3d4a4c0c2055d9cf9a2d1ea9541a5d9bdb0
Kirill Davydov <kirill_d1581@mail.ru@users.noreply.github.com>
2023-06-04 01:26:48 +0700

    Add more Tests

--- test_main.py
+++ test_main.py
@@ -19,6 +19,20 @@ class MainTest(unittest.TestCase):
         self.assertEqual(len(cw_arg.parameters), 2)
         self.assertEqual(len(ata_arg.parameters), 1)
         self.assertEqual(len(dp_arg.parameters), 2)
+    # Тест на тип массива
+    def test_ArrayType(self):
+        self.assertEqual(generate_enemy_ships(), True)
+    # Тест на параметры окна
+    def test_WindowSettings(self):
+        self.assertEqual(s_x, 10)
+        self.assertEqual(s_y, 10)
+        self.assertEqual(step_x, 50)
+        self.assertEqual(step_y, 50)
+        self.assertEqual(ships, 10)
+        self.assertEqual(ship_len1, 1)
+        self.assertEqual(ship_len2, 2)
+        self.assertEqual(ship_len3, 3)
+        self.assertEqual(ship_len4, 4)
 
 if __name__ == '__main__':
     unittest.main()
